4b515f66a18ea506632e13711d64ace97e6d88ec
AZ19999 <zhualexander@yahoo.com>
2023-02-18 12:41:53 -0500

    fixed some spellling mistakes causing bugs

--- Homework/CPA4/index.js
+++ Homework/CPA4/index.js
@@ -19,6 +19,12 @@ router.get("/", (request, response) => {
     response.end("Posted!");
   
   });
+
+  router.get("/index.html", (request, response) => {
+    response.writeHead(200, contentType.htmlType);
+    files.customReadFile("./views/index.html", response);
+  
+  });
   
   router.get("/contact.html", (request, response) => {
     response.writeHead(200, contentType.htmlType);
@@ -28,13 +34,19 @@ router.get("/", (request, response) => {
   
   router.get("/events.html", (request, response) => {
     response.writeHead(200, contentType.htmlType);
-    files.scustomReadFile("./views/events.html", response);
+    files.customReadFile("./views/events.html", response);
   
   });
 
-  router.get("/index.html", (request, response) => {
+  router.get("/about.html", (request, response) => {
     response.writeHead(200, contentType.htmlType);
-    files.customReadFile("./views/index.html", response);
+    files.customReadFile("./views/about.html", response);
+  
+  });
+
+    router.get("/jobs.html", (request, response) => {
+    response.writeHead(200, contentType.htmlType);
+    files.customReadFile("./views/jobs.html", response);
   
   });
   
@@ -46,7 +58,7 @@ router.get("/", (request, response) => {
 
   router.get("/BrandeisLogo.png", (request, response) => {
     response.writeHead(200, contentType.pngType);
-    files.customReadFile("./public/images/BrandesiLogo.png", response);
+    files.customReadFile("./public/images/BrandeisLogo.png", response);
   
   });
 
